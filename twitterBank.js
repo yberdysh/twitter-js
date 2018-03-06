5a5b44d2fee9757de913f5003ba106f3b3db54e3
Jane Costa <yberdysh@gmail.com>
2018-03-06 14:32:01 -0500

    completed twitterBank

--- twitterBank.js
+++ twitterBank.js
@@ -1,1 +1,49 @@
 //holds tweets and allows us to interact with them
+const _ = require('lodash');
+const data = [];
+//stores tweets
+
+function add (name, content) {
+  data.push({ name: name, content: content });
+}
+
+function list () {
+  return _.cloneDeep(data);
+}
+
+function find (properties) {
+  // console.log(properties);
+  // console.log(data);r
+  let arr =  _.cloneDeep(_.filter(data, properties));
+
+  return arr;
+}
+
+module.exports = { add: add, list: list, find: find };
+
+
+const randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const getFakeName = function() {
+  const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
+  const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+const getFakeTweet = function() {
+  const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+for (let i = 0; i < 10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
+
+add("Harry Potter", "Goblins!!!! Merlin\'s beard!");
+
+console.log(find({name :"Harry Potter"}));
+//find expects an object as a param
+
+
